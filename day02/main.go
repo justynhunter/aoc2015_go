e67027b7c62366746289791904f949668b59d81e
Justyn Hunter <cjhunter@me.com>
2024-07-25 21:36:28 -0500

    day 2 done

---
+++ day02/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/justynhunter/aoc2015_go/util"
+)
+
+func main() {
+	input := util.ReadFile("./input.txt")
+
+	boxDimensions := getBoxDimensions(input)
+
+	solution1 := solvePart1(boxDimensions)
+	fmt.Println("Day 2 Part 1:", solution1)
+
+	solution2 := solvePart2(boxDimensions)
+	fmt.Println("Day 2 Part 2:", solution2)
+}
+
+type BoxDimensions struct {
+	width  int
+	height int
+	length int
+}
+
+func getBoxDimensions(input string) []BoxDimensions {
+	lines := strings.Split(input, "\n")
+
+	var boxDimensions []BoxDimensions
+	for _, line := range lines {
+		dims := strings.Split(line, "x")
+		w, err := strconv.Atoi(dims[0])
+		if err != nil {
+			break
+		}
+
+		h, err := strconv.Atoi(dims[1])
+		if err != nil {
+			break
+		}
+
+		l, err := strconv.Atoi(dims[2])
+		if err != nil {
+			break
+		}
+
+		boxDimensions = append(boxDimensions, BoxDimensions{width: w, height: h, length: l})
+	}
+
+	return boxDimensions
+}
+
+func solvePart1(boxDimensions []BoxDimensions) int {
+	totalPaperNeeded := 0
+	for _, dim := range boxDimensions {
+		wh := dim.width * dim.height
+		wl := dim.width * dim.length
+		hl := dim.height * dim.length
+
+		totalPaperNeeded += 2*wh + 2*wl + 2*hl + min(wh, min(wl, hl))
+	}
+
+	return totalPaperNeeded
+}
+
+func solvePart2(boxDimensions []BoxDimensions) int {
+	totalRibbon := 0
+	for _, dim := range boxDimensions {
+		bow := dim.width * dim.height * dim.length
+		ribbon := 2 * slices.Min([]int{dim.width + dim.height, dim.width + dim.length, dim.height + dim.length})
+
+		totalRibbon += ribbon + bow
+	}
+
+	return totalRibbon
+}
